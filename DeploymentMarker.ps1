02c7e9dd16b67ce48ae6d28456be7bc1281a9b52
Noman Sadiq <nomi.cs786@gmail.com>
2019-06-13 16:19:11 +0400

    PS file having function to run powershell mode

---
+++ DeploymentMarker.ps1
@@ -0,0 +1,16 @@
+﻿
+
+
+
+
+Function Marker([string]$revision)
+{
+    Invoke-WebRequest -Uri https://api.newrelic.com/v2/applications/APPID/deployments.json -Method POST -Headers @{'X-Api-Key'='APIKey'} -ContentType 'application/json' -Body '{
+        "deployment": {
+        "revision": $revision,
+        "changelog": "testing",
+        "description": "testing",
+        "user": "noman.sadiq@osn.com"
+    }
+    }'
+}
