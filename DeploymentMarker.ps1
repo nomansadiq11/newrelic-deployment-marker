e72d4ff8c4794a3c738f75e05d409c2768f72c17
Noman Sadiq <nomi.cs786@gmail.com>
2019-06-19 13:54:36 +0400

    update script as function

--- DeploymentMarker.ps1
+++ DeploymentMarker.ps1
@@ -1,33 +1,33 @@
 
+Function DPMARKER()
+{
+    
 
+    $deployment = @{
+        "revision" = $env:RELEASE_RELEASENAME
+        "changelog" = $env:SYSTEM_DEFINITIONNAME
+        "description" = $env:SYSTEM_DEFINITIONNAME
+        "user" = $env:RELEASE_REQUESTEDFOREMAIL
+    }
 
-$revision = $env:RELEASE_RELEASENAME
-$user = $env:RELEASE_REQUESTEDFOREMAIL 
+    $Body = @{
+        "deployment" = $deployment
+    }
 
+    $JSON = $Body | convertto-json
 
+    
 
-$deployment = @{
-    "revision" = $revision
-    "changelog" = "some test"
-    "description" = "some test"
-    "user" = $user
+    $header = @{
+
+    "X-Api-Key" = $env:NewRelicAPIKey
+
+    }
+
+    $appid = $env:APPID
+
+    Invoke-WebRequest  https://api.newrelic.com/v2/applications/$appid/deployments.json -Method POST -Headers $header -ContentType 'application/json' -Body $JSON 
+
 }
 
-$Body = @{
-    "deployment" = $deployment
-}
-
-$JSON = $Body | convertto-json
-
-
-$header = @{
-
-"X-Api-Key" = $env:APIKey
-
-}
-
-$appid = $env:APPID
-
-Invoke-WebRequest  https://api.newrelic.com/v2/applications/$appid/deployments.json -Method POST -Headers $header -ContentType 'application/json' -Body $JSON 
-
-
+DPMARKER
