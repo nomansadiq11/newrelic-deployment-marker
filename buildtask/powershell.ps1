d01d12b090c59484ca299307e79b2e692d87b93f
Noman Sadiq <noman.sadiq@osn.com>
2019-07-09 17:50:51 +0400

    update powersheell for task

--- buildtask/powershell.ps1
+++ buildtask/powershell.ps1
@@ -3,10 +3,45 @@ param()
  
 Trace-VstsEnteringInvocation $MyInvocation 
 try { 
+    
+    
     # Get inputs. 
-    $inputName = Get-VstsInput -Name 'name' -Require 
+    $APIKey = Get-VstsInput -Name 'APIKey' -Require 
+    $APPID = Get-VstsInput -Name 'APPID' -Require 
+    $Revision = Get-VstsInput -Name 'Revision' -Require 
+    $changelog = Get-VstsInput -Name 'changelog' -Require 
+    $description = Get-VstsInput -Name 'description' -Require 
+
+
+    $deployment = @{
+        "revision" = $Revision
+        "changelog" = $changelog
+        "description" = $description
+        "user" = $env:RELEASE_REQUESTEDFOREMAIL
+    }
+
+    $Body = @{
+        "deployment" = $deployment
+    }
+
+    $JSON = $Body | convertto-json
+
+    write-host $JSON 
+
+    $header = @{
+
+    "X-Api-Key" = $APIKey
+
+    }
+
+    $appid = $APPID
+
+    Invoke-WebRequest  https://api.newrelic.com/v2/applications/$appid/deployments.json -Method POST -Headers $header -ContentType 'application/json' -Body $JSON 
      
-    Write-Host "Hello $inputName" 
+    
+
+
+
 } finally { 
     Trace-VstsLeavingInvocation $MyInvocation 
 }
